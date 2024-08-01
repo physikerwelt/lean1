53c062a4630a715b1e53639ce478d745de6c4db0
physikerwelt <wiki@physikerwelt.de>
2024-08-01 15:01:46 +0200

    Inital commit

---
+++ lakefile.lean
@@ -0,0 +1,21 @@
+import Lake
+open Lake DSL
+
+package "Untitled" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «Untitled» where
+  -- add any library configuration options here
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
